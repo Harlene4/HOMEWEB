390d5a0e712bc8808a6f429f86b143cce93bc1b8
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-25 22:42:56 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>COVID PANDEMIC </title>
+</head>
+<body> 
+ <p align="center"> <body background= "https://dev.rodpub.com/images/218/411_main.jpg" , width = 1200 , length = 800> </img>
+    
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<marquee> <b> <font color="lightgreen",size= "50"><h2> WELCOME TO OUR WEBSITE</h2> </font> </b> </marquee> 
+<style>
+h1 {
+  color: white;
+  text-shadow: 1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue;
+}
+</style>
+<center><b><font color="white", size= "48"><h1>The Global<br>Pandemic</h1> </font> </b></center>
+<center><b><font color= "white" , size= "20"> GROUP#6 </font> </center>
+<p align="center"> <img src = "https://i.pinimg.com/originals/1c/6a/8b/1c6a8b1c629f18ddfc469bccb54b4f45.jpg" , width = 210 , length = 210></img><br>
+ </body>
+</html>
